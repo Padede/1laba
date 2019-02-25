2ceb86eafb5e4dd3d60a5e2e1af1b8eca039dfda
Padede <avyf23@mail.ru>
2019-02-25 17:14:39 +0300

    after method

--- 1laba/1laba/Program.cs
+++ 1laba/1laba/Program.cs
@@ -6,18 +6,50 @@ using System.Threading.Tasks;
 
 namespace _1laba
 {
+
+    class Address
+    {
+        private string city;
+        public string City { get; set; }
+        private string street;
+        public string Street { get; set; }
+        private int home_number;
+        public int Home_number { get; set; }
+
+        public void print()
+        {
+            Console.WriteLine("Город:" + city + ", " + "Улица:" + street + ", " + "Номер дома:" + home_number);
+        }
+
+        public void setting()
+        {
+            Console.WriteLine("Введите город");
+            city = Console.ReadLine();
+            Console.WriteLine("Введите улицу");
+            street = Console.ReadLine();
+            Console.WriteLine("Введите номер дома");
+            home_number = int.Parse(Console.ReadLine());
+        }
+
+        public Address()
+        {
+            city = "Новополоцк";
+            street = "Молодежная";
+            home_number = 4;
+        }
+
+    }
+
+    
+
     class Program
     {
-        class Address
-        {
-            public string city;
-            public string street;
-            public int home_number;
-            
-        }
 
         static void Main(string[] args)
         {
+            Address a = new Address();
+            a.print();
+            Console.ReadLine();
         }
     }
 }
